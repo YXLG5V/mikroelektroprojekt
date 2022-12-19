b710e57ee916bf5cf468da1f8309258d25f803b5
YXLG5V <ratkai.robert.d@gmail.com>
2022-12-19 09:29:04 +0100

    api updated

--- code/api.py
+++ code/api.py
@@ -1,4 +1,5 @@
 from flask import Flask, json
+from flask_cors import CORS, cross_origin
 import redis
 
 db = redis.Redis("localhost")
@@ -6,12 +7,20 @@ DATA = [0, {"celsius": 0, "farenheit": 0, "status": "LOW/HIGH"}]
 stream_name="mystream"
 
 api = Flask(__name__)
+cors = CORS(api)
+api.config['CORS_HEADERS'] = 'Content-Type'
 
 @api.route('/gettemp', methods=['GET'])
+@cross_origin()
 def get_temp():
-  DATA = db.xrevrange(stream_name, "+", "-", count=1)
-  print(json.dumps(DATA[0][1]))
-  return json.dumps(DATA[0][1])
+  DATA = db.xrevrange(stream_name, "+", "-", count=10)
+  senddata = []
+  for x in reversed(DATA):
+        x = json.dumps(x[1])
+        x = json.loads(x)
+        senddata.append(float(x["celsius"]))
+  print(json.dumps(senddata))
+  return json.dumps(senddata)
 
 if __name__ == '__main__':
     api.run()
